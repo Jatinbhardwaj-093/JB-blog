02aa6de1de718d59cffd2063b0e61c9c5a07ddc4
Jatin Bhardwaj <bhardwajjatin093@gmail.com>
2025-02-01 19:55:34 +0530

    sdf

--- src/Blogs/SympyDiffGeom_Module.vue
+++ src/Blogs/SympyDiffGeom_Module.vue
@@ -3,7 +3,7 @@
   <!-- Content -->
     <div class="w-[60vw] h-screen p-4 border-x-2 border-green-300 bg-grid">
       <header>
-        <h1 class="text-4xl font-bold text-green-600 font-raleway text-center mt-4 mb-0.5">Sympy Differential Geometry Module</h1>
+        <h1 class="text-4xl font-bold text-green-600 font-raleway text-center mt-4 mb-0.5">Sympy PDE Module</h1>
         <p class="text-sm text-black font-raleway text-right">January 2025</p>
         <div class="w-[98%] h-0.5 bg-gray-300 mx-auto mb-4"></div>
       </header>
